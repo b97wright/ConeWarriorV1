79a3306576b7bde7b11b2ce6519c2ed896f24a26
b97wright <110114784+b97wright@users.noreply.github.com>
2024-08-29 23:50:20 -0400

    Initial commit

---
+++ Source/ConeWarriorV1/ConeWarriorV1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ConeWarriorV1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ConeWarriorV1, "ConeWarriorV1" );
